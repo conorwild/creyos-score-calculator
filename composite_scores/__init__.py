11b7f385d390fdb8b18ee72367323fbf56e47969
Conor Wild <conor.wild@cambridgebrainsciences.com>
2024-07-10 11:42:53 -0400

    SCI-133 fixed import

--- composite_scores/__init__.py
+++ composite_scores/__init__.py
@@ -1,4 +1,4 @@
-from .models import (
+from .sklearn_ext import (
     ColumnSelector,
     CompositeScores,
     DomainScores,
--- notebooks/train_pipeline.py
+++ notebooks/train_pipeline.py
@@ -17,8 +17,9 @@ df_ = DomainScores.required_features()
 tf_ = ProcessingSpeed.required_features()
 
 comp_score_calc = CompositeScores(overall_features=df_).fit(Ynorm[df_ + tf_])
-comp_score_calc.domains.names = ["STM", "Reasoning", "Verbal"]
+comp_score_calc.domains.names = ["STM", "reasoning", "verbal"]
 display(comp_score_calc.domains.loadings)  # pyright: ignore
+display(comp_score_calc.processing_speed.loadings)  # pyright: ignore
 comp_score_calc.save_pretrained()
 
 # %%
